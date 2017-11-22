96b39db5d993c09a2e0b0d8059c8a0caad2e48a6
Morio Ramdenbourg <morio.ramdenbourg@gmail.com>
2017-11-22 00:31:26 -0600

    Modifications to try.py to allow for only one to be selected at a time

--- try.py
+++ try.py
@@ -1,10 +1,48 @@
-#Jae Lee & Cameron Moeller
-#sample input line
-#python try.py usename password
+"""
+Jae Lee & Cameron Moeller
+sample input line
+python try.py usename password
+"""
 
+from enum import Enum
 import mechanize
 import sys
+import json
 
+
+# enum for the sites
+class Site(str, Enum):
+    Facebook = "facebook"
+    Zipcar = "zipcar"
+    Ebay = "ebay"
+    Tumblr = "tumblr"
+    Netflix = "netflix"
+
+# Dictionary of sites that we can attempt to authenticate
+urls = {
+    Site.Facebook: {
+        "attempt": "http://www.facebook.com/login.php",
+        "correct": "https://www.facebook.com/?sk=welcome"
+    },
+    Site.Zipcar: {
+        "attempt": "https://members.zipcar.com/register/",
+        "correct": "https://members.zipcar.com/members/account"
+    },
+    Site.Ebay: {
+        "attempt": "https://signin.ebay.com/ws/eBayISAPI.dll?SignIn&ru=https%3A%2F%2Fwww.ebay.com%2F",
+        "correct": "https://www.ebay.com/"
+    },
+    Site.Tumblr: {
+        "attempt": "https://www.tumblr.com/login",
+        "correct": "https://www.tumblr.com/dashboard"
+    },
+    Site.Netflix: {
+        "attempt": "https://www.netflix.com/login",
+        "correct": "https://www.netflix.com/browse"
+    }
+}
+
+# Browser settings
 browser = mechanize.Browser()
 browser.set_handle_robots(False)
 cookies = mechanize.CookieJar()
@@ -12,87 +50,61 @@ browser.set_cookiejar(cookies)
 browser.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 browser.set_handle_refresh(False)
 
-def main():
-    #test login pages
-    try_url = ["http://www.facebook.com/login.php",
-                "https://members.zipcar.com/register/",
-                "https://signin.ebay.com/ws/eBayISAPI.dll?SignIn&ru=https%3A%2F%2Fwww.ebay.com%2F",
-                "https://www.tumblr.com/login",
-                "https://www.netflix.com/login"
-                ]
 
-    correct_url = ["https://www.facebook.com/",
-                    "https://members.zipcar.com/members/account",
-                    "https://www.ebay.com/",
-                    "https://www.tumblr.com/dashboard",
-                    "https://www.netflix.com/browse"]
+def main():
 
     # fb #need to use nr = 0 (email, pass)
-
     # zipcar #need to use nr = 0 (user_name, password)
-
     # ebay #need to use nr = 0 (userid, pass)
-
     # tumblr #need to use nr = 0 (user[email], user[password])
-
     # netflix #need to use nr = 0 (email, password)
 
-    username = sys.argv[1]
-    password = sys.argv[2]
+    try:
+        # Grab command line arguments, exit if it is invalid
+        site = sys.argv[1]
+        username = sys.argv[2]
+        password = sys.argv[3]
 
-#try the credentials on different sites
-    response_url_collection = list()
-    for i in try_url:
-        browser.open(i)
-        browser.select_form(nr = 0)       #This is login-password form -> nr = number = 0
-        if "facebook" in i:
-            browser.form['email'] = username
-            browser.form['pass'] = password
-            response = browser.submit()
-        if "zipcar" in i:
-            browser.form['user_name'] = username
-            browser.form['password'] = password
-            response = browser.submit()
-        if "ebay" in i:
-            browser.form['userid'] = username
-            browser.form['pass'] = password
-            response = browser.submit()
-        if "tumblr" in i :
-            browser.form['user[email]'] = username
-            browser.form['user[password]'] = password
-            response = browser.submit()
-        if "netflix" in i:
-            browser.form['email'] = username
-            browser.form['password'] = password
-            response = browser.submit()
+        # Authenticate to site
+        result = attempt_authentication(site=site, username=username, password=password)
+        print(json.dumps(result))
 
-        response_url = response.geturl()
-        response_url_collection.append(response_url)
-    print ("resulting urls ", response_url_collection)
-
-#compare the urls
-    match_success_urls = list()
-    for j in response_url_collection:
-        for h in correct_url:
-            if j == h:
-                match_success_urls.append(j)
-
-#clean up url to site names
-    sites_names = list()
-    for y in match_success_urls:
-        if "facebook" in y:
-            sites_names.append("facebook")
-        if "zipcar" in y:
-            sites_names.append("zipcar")
-        if "ebay" in y:
-            sites_names.append("ebay")
-        if "tumblr" in y:
-            sites_names.append("tumblr")
-        if "netflix" in y:
-            sites_names.append("netflix")
-    print ("sites we were able to access : " , sites_names)
+    except Exception as e:
+        # Exit program and print an error
+        error = {"error": e.args}
+        print(json.dumps(error))
 
 
+def attempt_authentication(site, username, password):
+    # grab fields
+    attempt = urls[site]["attempt"]
+    correct = urls[site]["correct"]
+    browser.open(attempt)
+    browser.select_form(nr=0)
+
+    # try the credentials on different sites
+    if site == Site.Facebook:
+        browser.form['email'] = username
+        browser.form['pass'] = password
+    if site == Site.Zipcar:
+        browser.form['user_name'] = username
+        browser.form['password'] = password
+    if site == Site.Ebay:
+        browser.form['userid'] = username
+        browser.form['pass'] = password
+    if site == Site.Tumblr:
+        browser.form['user[email]'] = username
+        browser.form['user[password]'] = password
+    if site == Site.Netflix:
+        browser.form['email'] = username
+        browser.form['password'] = password
+
+    # Submit the request and get the response
+    response = browser.submit()
+    response_url = response.geturl()
+
+    # Check if the authentication was successful
+    return {site: True} if response_url == correct else {site: False}
 
 if __name__ == '__main__':
     main()
