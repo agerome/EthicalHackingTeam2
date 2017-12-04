69d8428f87b84f98e7839a5703db4297deb7fb57
agerome <alex.gerome@utexas.edu>
2017-12-04 14:26:33 -0600

    Added a vanilla email script

---
+++ Email/emailResults.py
@@ -0,0 +1,25 @@
+import yagmail
+import sys
+'''
+USAGE:
+python emailer.py <email-addr> <report.txt>
+'''
+
+#yag = yagmail.SMTP('''FILL THIS OUT WITH YOUR USER/PASS FOR YOUR EMAIL SERVICE''')
+#yag = yagmail.SMTP('<USER>', '<PASS>')
+
+
+#load email from HTML file
+'''TODO:
+make sure email picture is from the user email fb acct.
+make sure user name is correct ($USER -> fb profile name)
+move subject line to a new file maybe?
+'''
+email = sys.argv[1]
+with open(sys.argv[2], 'r') as f:
+    data = f.read()
+    contents = [data]
+    subject = 'Phishing Results'
+    yag.send(email, subject, contents)
+
+print('Results sent to ' + email)
